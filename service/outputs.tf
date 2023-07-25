66b7e33b9262a9777f8f9fd426daad5630a36835
Kristopher Landon <landonkt@Kristophers-MacBook-Pro-2.local>
2023-07-25 12:18:25 -0400

    updating outputs for hostname

--- service/outputs.tf
+++ service/outputs.tf
@@ -4,3 +4,7 @@ output "app_service_id" {
 output "app_service_name" {
   value = azurerm_linux_web_app.web-app.name
 }
+
+output "app_service_hostname" {
+  value = azurerm_linux_web_app.web-app.default_hostname
+}
